d50b022a6edc97648bdf2ffb3e8d41374cf11d6e
Mateusz_Staniszewski <mateusz_staniszewski@epam.com>
2023-05-27 00:13:33 +0200

    Initial commit

---
+++ src/main/java/com/mjakubsky/ecommstore/EcommStoreApplication.java
@@ -0,0 +1,13 @@
+package com.mjakubsky.ecommstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjakubsky/ecommstore/EcommStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjakubsky.ecommstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
